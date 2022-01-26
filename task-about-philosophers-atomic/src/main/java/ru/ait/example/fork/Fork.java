671fe8c45eec4514419ddbde8468fea4fae1da5e
Anton.Tonkikh <Anton.Tonkikh@x5.ru>
2022-01-26 10:55:14 +0500

    add atomic

---
+++ task-about-philosophers-atomic/src/main/java/ru/ait/example/fork/Fork.java
@@ -0,0 +1,25 @@
+package ru.ait.example.fork;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Fork {
+
+  private AtomicBoolean free = new AtomicBoolean(true);
+  private final int number;
+
+  public Fork(int number) {
+    this.number = number;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public boolean take() throws InterruptedException {
+    return free.compareAndSet(true, false);
+  }
+
+  public void put() {
+    free.compareAndSet(false, true);
+  }
+}
